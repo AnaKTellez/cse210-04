c94527275ae4f007af8d0e582f8dc10949084214
roj21008 <roj21008@byui.edu>
2022-03-26 18:39:16 -0500

    aa

--- greed/game/casting/score.py
+++ greed/game/casting/score.py
@@ -4,16 +4,14 @@ from game.casting.actor import Actor
 class Score(Actor):
     """
     An item of cultural or historical interest. 
-    
-    The responsibility of an Artifact is to provide a message about itself.
-    Attributes:
-        _message (string): A short description about the artifact.
+  
+        
     """
     def __init__(self):
         super().__init__()
         self._points = 0
 
-    def get_points(self):
+    def get_points(self): 
         """Returns the current score.
         
         Returns:
